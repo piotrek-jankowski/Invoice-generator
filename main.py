595c8a3a3a6cdec81d491e9eb51338624cf3f076
Piotr Jankowski <piotr.jankowski@zapr.pl>
2022-11-24 09:54:34 +0100

    Connect "invoice setup" buttons to file dialog windows

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functools import partial
 import sys
 from PySide6.QtWidgets import QApplication, QFileDialog
+from PySide6.QtCore import QStandardPaths
 from Views.screen_ex import Screen
 
 
@@ -13,10 +14,16 @@ class Invoice_generator:
 
         
     def connect_buttons(self):
-        self.screen_window.button_template.clicked.connect(self.browse_files)
+        self.screen_window.button_template.clicked.connect(self.open_file_template)
+        self.screen_window.button_invoice.clicked.connect(self.open_file_data)
+        self.screen_window.button_destination.clicked.connect(self.open_file_destination)
 
-    def browse_files(self):
-        file_name = QFileDialog.getOpenFileName(None, "Open File", ".", "Invoice Template(*_template.docx)")
+    def open_file_template(self):
+        self.file_template = QFileDialog.getOpenFileName(None, "Select template file", QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation), "Microsoft Word file (*_template.docx)")
+    def open_file_data(self):
+        self.file_data = QFileDialog.getOpenFileName(None, "Select data file", QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation), "Data source files (*.xml *.json)")
+    def open_file_destination(self):
+        self.file_destination = QFileDialog.getSaveFileName(None, "Save destination file", QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation), "Microsoft Word file (*.docx)")
 
 
 if __name__ == "__main__":
